281b12797e3040f1b6916ce2d3d8519cbfedd929
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2024-07-04 00:47:22 +0900

    USe Postgres

--- chat_with_mp/settings.py
+++ chat_with_mp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +79,8 @@ WSGI_APPLICATION = 'chat_with_mp.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 
 # Password validation
